7e5d03f12057f6f8ceac1a650b66060b1db7fe1b
Mo7amed7assan <mo7amed7assan1911@gmail.com>
2025-01-11 19:13:13 +0200

    enforce the implementation of the abstract methods in the base provider

--- LLM_Providers/base_provider.py
+++ LLM_Providers/base_provider.py
@@ -1,8 +1,10 @@
-class BaseModelProvider:
+from abc import ABC, abstractmethod
+class BaseModelProvider(ABC):
     def __init__(self, model_name, max_tokens=250, temperature=0.4):
         self.model_name = model_name
         self.max_tokens = max_tokens
         self.temperature = temperature
 
+    @abstractmethod
     def get_response(self, prompt):
         raise NotImplementedError("Subclass must implement abstract method")
